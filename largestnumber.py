8e46969022b77181768269d27da031d4af699cb9
MerinAlex23 <merinalex2323@gmail.com>
2019-10-28 09:40:18 +0530

    finding largest number

---
+++ largestnumber.py
@@ -0,0 +1,19 @@
+numbers = [0, 2, 3, 9, 6]
+max = numbers[2]
+for number in numbers:
+     if number > max:
+         max = number
+print(max)
+
+
+jio = [5, 2, 5, 2, 2]
+for pop in jio:
+    output = ""
+    for f in range(pop):
+     output = output+"x"
+    print(output)
+print(jio)
+matrix [
+    
+]
+
